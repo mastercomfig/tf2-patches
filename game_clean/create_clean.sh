ef502680c3c761631c5093712240afe8ddb4ff29
mastercoms <mastercoms@tuta.io>
2020-08-15 00:08:40 -0400

    create script for release
    add -nomaster
    use voice_steal 2
    hush NO_AUTO_P4

---
+++ game_clean/create_clean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Run script within the directory
+BINDIR=$(dirname "$(readlink -fn "$0")")
+cd "${BINDIR}" || exit 2
+
+set -e
+
+clean_folder=../../game_clean
+dev_folder=../../game
+mkdir -p $clean_folder
+mkdir -p $clean_folder/bin
+mkdir -p $clean_folder/tf/bin
+mkdir -p $clean_folder/tf/scripts/items
+cp -f *.bat $clean_folder
+
+declare -a files=("steam_appid.txt" "hl2.exe" "srcds.exe" "bin/engine.dll" "bin/GameUI.dll" "bin/replay.dll" "bin/launcher.dll" "bin/inputsystem.dll" "bin/MaterialSystem.dll" "bin/dedicated.dll" "bin/shaderapidx9.dll" "bin/vguimatsurface.dll" "bin/stdshader_dx9.dll" "bin/vgui2.dll" "bin/datacache.dll" "bin/sourcevr.dll" "bin/StudioRender.dll" "bin/SoundEmitterSystem.dll" "bin/bsppack.dll" "bin/FileSystem_Stdio.dll" "bin/scenefilecache.dll" "bin/vstdlib.dll" "bin/tier0.dll" "tf/bin/client.dll" "tf/bin/server.dll")
+for F in "${files[@]}"; do
+  cp -f $dev_folder/$F $clean_folder/$F
+done
--- game_clean/srcds.bat
+++ game_clean/srcds.bat
@@ -1,2 +1,2 @@
 @echo off
-srcds.exe -console -game tf %*
+srcds.exe -nomaster -console -game tf %*
