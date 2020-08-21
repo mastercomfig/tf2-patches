2da19384fc900d902af5d274523ead0cb82384aa
mastercoms <mastercoms@tuta.io>
2020-08-21 12:56:29 -0400

    add new game clean files

--- game_clean/create_clean.sh
+++ game_clean/create_clean.sh
@@ -11,6 +11,8 @@ mkdir -p $clean_folder
 mkdir -p $clean_folder/bin
 mkdir -p $clean_folder/tf/bin
 cp -f *.bat $clean_folder
+cp -f *.dll $clean_folder
+cp -rf tc2/ $clean_folder
 
 declare -a files=("steam_appid.txt" "hl2.exe" "srcds.exe" "bin/engine.dll" "bin/GameUI.dll" "bin/replay.dll" "bin/launcher.dll" "bin/inputsystem.dll" "bin/MaterialSystem.dll" "bin/dedicated.dll" "bin/ServerBrowser.dll" "bin/shaderapidx9.dll" "bin/vguimatsurface.dll" "bin/stdshader_dx9.dll" "bin/vgui2.dll" "bin/datacache.dll" "bin/sourcevr.dll" "bin/StudioRender.dll" "bin/SoundEmitterSystem.dll" "bin/bsppack.dll" "bin/FileSystem_Stdio.dll" "bin/scenefilecache.dll" "bin/vstdlib.dll" "bin/tier0.dll" "tf/bin/client.dll" "tf/bin/server.dll")
 for F in "${files[@]}"; do
