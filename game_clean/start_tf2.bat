70785fe30b9845d17f0af449a0cc4dc117e8ae01
mastercoms <mastercoms@tuta.io>
2020-09-05 19:16:16 -0400

    clean up scripts

--- game_clean/start_tf2.bat
+++ game_clean/start_tf2.bat
@@ -1,2 +1,2 @@
 @echo off
-hl2.bat -novid -nojoy -nosteamcontroller -nohltv -particles 1 -noborder -particle_fallback 2 %* < nul
+hl2.bat -novid -nojoy -nosteamcontroller -nohltv -particle_fallback 2 %* < nul
