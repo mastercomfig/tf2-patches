715a1829efd4602868851da5f94559b948f92991
Maksim Smolin <maximsmol@gmail.com>
2020-09-15 12:58:57 -0700

    add dependency on i686 glibc

--- shell.nix
+++ shell.nix
@@ -1,10 +1,11 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? (import <nixpkgs> {}).pkgsi686Linux }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
     gcc
     gnumake
 
+    glib
     libunwind
 
     # keep this line if you use bash
