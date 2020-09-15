mastercomfig/tf2-patches

commit 3a984203002dbd2c89db5bffe011e4e98d62e7af

VGUI/mouse opt
threading distribution opt

game/client/in_mouse.cpp
@@ -134,7 +134,7 @@ void CInput::ActivateMouse (void)
 		}
 		m_fMouseActive = true;
 
-		ResetMouse();
+		//ResetMouse();
 #if !defined( PLATFORM_WINDOWS )
 		int dx, dy;
 		engine->GetMouseDelta( dx, dy, true );
@@ -683,7 +683,7 @@ void CInput::MouseMove( CUserCmd *cmd )
 		ApplyMouse( viewangles, cmd, mouse_x, mouse_y );
 
 		// Re-center the mouse.
-		ResetMouse();
+		//ResetMouse();
 	}
 
 	// Store out the new viewangles.

inputsystem/inputsystem.cpp
@@ -305,6 +305,14 @@ void CInputSystem::AttachToWindow( void* hWnd )
 
 	m_hAttachedHWnd = (HWND)hWnd;
 
+#if defined( PLATFORM_WINDOWS )
+	POINT pt;
+	pt.x = 0; pt.y = 0;
+	ClientToScreen((HWND)m_hAttachedHWnd, &pt);
+	m_windowOffsetX = pt.x;
+	m_windowOffsetY = pt.y;
+#endif
+
 #if defined( PLATFORM_WINDOWS_PC ) && !defined( USE_SDL )
 	// NVNT inform novint devices of window
 	AttachWindowToNovintDevices( hWnd );
@@ -1202,8 +1210,7 @@ void CInputSystem::SetCursorPosition( int x, int y )
 
 #if defined( PLATFORM_WINDOWS )
 	POINT pt;
-	pt.x = x; pt.y = y;
-	ClientToScreen( (HWND)m_hAttachedHWnd, &pt );
+	pt.x = x + m_windowOffsetX; pt.y = y + m_windowOffsetY;
 	SetCursorPos( pt.x, pt.y );
 #elif defined( USE_SDL )
 	m_pLauncherMgr->SetCursorPosition( x, y );

vguimatsurface/MatSystemSurface.cpp
@@ -4089,6 +4089,8 @@ void CMatSystemSurface::CalculateMouseVisible()
 	int c = surface()->GetPopupCount();
 
 	VPANEL modalSubTree = input()->GetModalSubTree();
+	VPanel* p = nullptr;
+
 	if ( modalSubTree )
 	{
 		for (i = 0 ; i < c ; i++ )
@@ -4099,7 +4101,7 @@ void CMatSystemSurface::CalculateMouseVisible()
 				continue;
 
 			bool isVisible=pop->IsVisible();
-			VPanel *p= pop->GetParent();
+			p= pop->GetParent();
 
 			while (p && isVisible)
 			{
@@ -4129,7 +4131,7 @@ void CMatSystemSurface::CalculateMouseVisible()
 			VPanel *pop = (VPanel *)surface()->GetPopup(i) ;
 			
 			bool isVisible=pop->IsVisible();
-			VPanel *p= pop->GetParent();
+			p= pop->GetParent();
 
 			while (p && isVisible)
 			{
@@ -4158,6 +4160,9 @@ void CMatSystemSurface::CalculateMouseVisible()
 		// NOTE: We must unlock the cursor *before* the set call here.
 		// Failing to do this causes s_bCursorVisible to not be set correctly
 		// (UnlockCursor fails to set it correctly)
+#ifdef PLATFORM_WINDOWS
+		::ClipCursor(NULL);
+#endif
 		UnlockCursor();
 		if ( _currentCursor == vgui::dc_none )
 		{
@@ -4166,6 +4171,27 @@ void CMatSystemSurface::CalculateMouseVisible()
 	}
 	else
 	{
+#ifdef PLATFORM_WINDOWS
+		if (p)
+		{
+			VPanel* contextPanel = p;
+			while (contextPanel->GetParent() && !contextPanel->Plat())
+			{
+				contextPanel = contextPanel->GetParent();
+			}
+			int absThis[4];
+			contextPanel->GetAbsPos(absThis[0], absThis[1]);
+			contextPanel->GetSize(absThis[2], absThis[3]);
+			absThis[2] += absThis[0];
+			absThis[3] += absThis[1];
+			RECT rect;
+			rect.left = absThis[0];
+			rect.top = absThis[1];
+			rect.right = absThis[2];
+			rect.bottom = absThis[3];
+			::ClipCursor(&rect);
+		}
+#endif
 		SetCursor(vgui::dc_none);
 		LockCursor();
 	}

engine/host.cpp
@@ -462,7 +462,7 @@ void OnChangeThreadAffinity( IConVar *var, const char *pOldValue, float flOldVal
 {
 	if ( g_pThreadPool->NumThreads() )
 	{
-		g_pThreadPool->Distribute( threadpool_affinity.GetBool() );
+		g_pThreadPool->Distribute( threadpool_affinity.GetBool(), NULL, false );
 	}
 }
 

engine/vgui_baseui_interface.cpp
@@ -1660,7 +1660,7 @@ void CEngineVGui::Simulate()
 		VGui_UpdateDrawTreePanel();
 		VGui_UpdateTextureListPanel();
 
-		vgui::surface()->CalculateMouseVisible();
+		//vgui::surface()->CalculateMouseVisible();
 		VGui_ActivateMouse();
 	}
 

inputsystem/inputsystem.h
@@ -368,6 +368,8 @@ public:
 
 	WNDPROC m_ChainedWndProc;
 	HWND m_hAttachedHWnd;
+	int m_windowOffsetX;
+	int m_windowOffsetY;
 	bool m_bEnabled;
 	bool m_bPumpEnabled;
 	bool m_bIsPolling;

materialsystem/cmaterialsystem.cpp
@@ -3545,6 +3545,7 @@ IThreadPool *CMaterialSystem::CreateMatQueueThreadPool()
 		startParams.nThreads = 1;
 		startParams.nStackSize = 256*1024;
 		startParams.fDistribute = TRS_TRUE;
+		startParams.bFullCore = true;
 
 		// The rendering thread has the GL context and the main thread is coming in and
 		//  "helping" finish jobs - that breaks OpenGL, which requires TLS. This flag states 

public/vstdlib/jobthread.h
@@ -99,7 +99,7 @@ enum JobPriority_t
 struct ThreadPoolStartParams_t
 {
 	ThreadPoolStartParams_t( bool bIOThreads = false, unsigned nThreads = -1, int *pAffinities = NULL, ThreeState_t fDistribute = TRS_NONE, unsigned nStackSize = -1, int iThreadPriority = SHRT_MIN )
-		: bIOThreads( bIOThreads ), nThreads( nThreads ), fDistribute( fDistribute ), nStackSize( nStackSize ), iThreadPriority( iThreadPriority ), nThreadsMax( -1 )
+		: bIOThreads( bIOThreads ), nThreads( nThreads ), fDistribute( fDistribute ), nStackSize( nStackSize ), iThreadPriority( iThreadPriority ), nThreadsMax( -1 ), bFullCore(false)
 	{
 		bExecOnThreadPoolThreadsOnly = false;
 
@@ -125,6 +125,7 @@ struct ThreadPoolStartParams_t
 	bool			bIOThreads : 1;
 	bool			bUseAffinityTable : 1;
 	bool			bExecOnThreadPoolThreadsOnly : 1;
+	bool            bFullCore;
 };
 
 //-----------------------------------------------------------------------------
@@ -412,7 +413,7 @@ private:
 	virtual CJob *GetDummyJob() = 0;
 
 public:
-	virtual void Distribute( bool bDistribute = true, int *pAffinityTable = NULL ) = 0;
+	virtual void Distribute( bool bDistribute = true, int *pAffinityTable = NULL, bool bFullCore = true ) = 0;
 
 	virtual bool Start( const ThreadPoolStartParams_t &startParams, const char *pszNameOverride ) = 0;
 };

commit dd05550052d9f38d588012375e1986761fce048a

opt(threads): don't notify events if they're already signalled.
fix(core): fix licensing ambiguity: leaked code is not under the SDK license!
fix(input): fix bugs with ClipCursor functionality introduced in 3a98420 (fixes #66)

.gitignore
@@ -16,6 +16,7 @@ ipch
 *.opensdf
 *.idb
 *.sln
+*.recipe
 
 # OSX/Linux build products
 *.mak

LICENSE_SDK
@@ -1,3 +1,5 @@
+Parts of this repo contain the Source 1 SDK, the license of which is as follows:
+
                SOURCE 1 SDK LICENSE
 
 Source SDK Copyright(c) Valve Corp.  

engine/sys_mainwind.cpp
@@ -301,6 +301,9 @@ void CGame::AppActivate( bool fActive )
 			ClearIOStates();
 			
 			UpdateMaterialSystemConfig();
+
+			// Once we return, let VGUI know it needs to relock.
+			vgui::surface()->UnlockCursor();
 		}
 		else
 		{

game/client/in_mouse.cpp
@@ -170,9 +170,10 @@ void CInput::DeactivateMouse (void)
 		}
 		m_fMouseActive = false;
 		vgui::surface()->SetCursor( vgui::dc_arrow );
-#if !defined( PLATFORM_WINDOWS )
 		// now put the mouse back in the middle of the screen
 		ResetMouse();
+#ifdef PLATFORM_WINDOWS
+		::ClipCursor(NULL);
 #endif
 
 		// Clear accumulated error, too

game/client/tf/vgui/tf_matchmaking_dashboard.cpp
@@ -279,6 +279,8 @@ void CTFMatchmakingPopup::ApplySchemeSettings( IScheme *pScheme )
 	}
 
 	GetMMDashboardParentManager()->UpdateParenting();
+
+	SetKeyBoardInputEnabled(false); // Never
 }
 
 void CTFMatchmakingPopup::OnThink()
@@ -314,10 +316,8 @@ void CTFMatchmakingPopup::OnTick()
 			m_bActive = false;
 			OnExit();
 		}
+		SetMouseInputEnabled(ShouldBeActve());
 	}
-
-	SetMouseInputEnabled( ShouldBeActve() );
-	SetKeyBoardInputEnabled( false ); // Never
 }
 
 

inputsystem/inputsystem.cpp
@@ -305,7 +305,7 @@ void CInputSystem::AttachToWindow( void* hWnd )
 
 	m_hAttachedHWnd = (HWND)hWnd;
 
-#if defined( PLATFORM_WINDOWS )
+#if 0 && defined( PLATFORM_WINDOWS )
 	POINT pt;
 	pt.x = 0; pt.y = 0;
 	ClientToScreen((HWND)m_hAttachedHWnd, &pt);
@@ -1210,7 +1210,12 @@ void CInputSystem::SetCursorPosition( int x, int y )
 
 #if defined( PLATFORM_WINDOWS )
 	POINT pt;
+#if 0
 	pt.x = x + m_windowOffsetX; pt.y = y + m_windowOffsetY;
+#else
+	pt.x = x; pt.y = y;
+	ClientToScreen((HWND)m_hAttachedHWnd, &pt);
+#endif
 	SetCursorPos( pt.x, pt.y );
 #elif defined( USE_SDL )
 	m_pLauncherMgr->SetCursorPosition( x, y );

tier0/threadtools.cpp
@@ -623,15 +623,9 @@ bool CThreadEvent::Set()
         std::unique_lock<std::mutex> lock(m_Mutex);
 		if (m_bSignaled)
 		{
-			if (!m_bAutoReset)
-			{
-				return true;
-			}
+			return true;
 		}
-		else
-		{
-			m_bSignaled = true;
-		}
+		m_bSignaled = true;
     }
 	if (m_bAutoReset)
 	{

vguimatsurface/MatSystemSurface.cpp
@@ -4160,9 +4160,6 @@ void CMatSystemSurface::CalculateMouseVisible()
 		// NOTE: We must unlock the cursor *before* the set call here.
 		// Failing to do this causes s_bCursorVisible to not be set correctly
 		// (UnlockCursor fails to set it correctly)
-#ifdef PLATFORM_WINDOWS
-		::ClipCursor(NULL);
-#endif
 		UnlockCursor();
 		if ( _currentCursor == vgui::dc_none )
 		{
@@ -4172,7 +4169,7 @@ void CMatSystemSurface::CalculateMouseVisible()
 	else
 	{
 #ifdef PLATFORM_WINDOWS
-		if (p)
+		if (!IsCursorLocked() && p)
 		{
 			VPanel* contextPanel = p;
 			while (contextPanel->GetParent() && !contextPanel->Plat())