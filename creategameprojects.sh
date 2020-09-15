675bf86cbf25ec0c64138e5295fe2c6690fc354a
HurricanePootis <eatabakhsh04@protonmail.com>
2020-09-15 11:36:11 -0500

    Changed vpc to vpc_linux to make room for vpc_osx in the future. Changed creategameprojects.sh to refelct change. Made ccache marked as executable.

--- creategameprojects.sh
+++ creategameprojects.sh
@@ -1,1 +1,1 @@
-devtools/bin/vpc /2015 /define:WORKSHOP_IMPORT_DISABLE /define:SIXENSE_DISABLE /define:NO_X360_XDK /define:RAD_TELEMETRY_DISABLED /define:DISABLE_ETW /define:LTCG /no_ceg /retail /define:CERT /tf +game /mksln games.sln
+devtools/bin/vpc_linux /2015 /define:WORKSHOP_IMPORT_DISABLE /define:SIXENSE_DISABLE /define:NO_X360_XDK /define:RAD_TELEMETRY_DISABLED /define:DISABLE_ETW /define:LTCG /no_ceg /retail /define:CERT /tf +game /mksln games.sln
