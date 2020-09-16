c84ea2e6849a5fce7316b22f082745a4f80850b5
Nicolas Aragone <niquito.larigone@gmail.com>
2020-09-16 06:46:29 -0300

    Added SAL POSIX NULL fix from maximsmol, with proper guarding in DXMath.

---
+++ thirdparty/DirectXMath-apr2020/Native/sal_posix.h
@@ -0,0 +1,15 @@
+// https://github.com/microsoft/DirectXMath/issues/89
+
+#pragma once
+
+#define _In_
+#define _Out_
+#define _Out_opt_
+#define _Use_decl_annotations_
+#define _Analysis_assume_
+
+#define _Success_(x)
+#define _In_reads_(x)
+#define _In_reads_bytes_(x)
+#define _Out_writes_(x)
+#define _Out_writes_bytes_(x)
