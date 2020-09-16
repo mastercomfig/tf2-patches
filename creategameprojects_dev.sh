8e1ee880e1aee9a691df960ae2abc2ab5c05d212
melvyn2 <melvyn.depeyrot@gmail.com>
2020-09-15 14:53:54 -1000

    clarify that vpc2 is indeed vpc2

--- creategameprojects_dev.sh
+++ creategameprojects_dev.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 chmod u+x ./devtools/bin/vpc_linux
-devtools/bin/vpc_linux /define:WORKSHOP_IMPORT_DISABLE /define:SIXENSE_DISABLE /define:NO_X360_XDK /define:RAD_TELEMETRY_DISABLED /define:DISABLE_ETW /define:LTCG /no_ceg /retail /define:CERT /nofpo /tf +game /mksln games.mak
+devtools/bin/vpc2_linux /define:WORKSHOP_IMPORT_DISABLE /define:SIXENSE_DISABLE /define:NO_X360_XDK /define:RAD_TELEMETRY_DISABLED /define:DISABLE_ETW /define:LTCG /no_ceg /retail /define:CERT /nofpo /tf +game /mksln games.mak
