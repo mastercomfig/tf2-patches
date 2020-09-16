e6658a6965f05ff4e37137335587d3455c67b097
Maksim Smolin <maximsmol@gmail.com>
2020-09-15 17:47:51 -0700

    upd utils

--- build1.sh
+++ build1.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+if [[ -z "$1" ]]; then
+  echo 'Usage: build1.sh <project> [extra make args...]'
+fi
+
 export VALVE_NO_AUTO_P4=1
 cd "$1" && make -f "$1_linux32.mak" NO_CHROOT=1 "${@:2}"
--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 { pkgs ? (import <nixpkgs> {}).pkgsi686Linux }:
 
-pkgs.mkShell {
+let ogl = pkgs.linuxPackages.nvidia_x11;
+in pkgs.mkShell {
   buildInputs = with pkgs; [
     gcc
     gnumake
@@ -9,6 +10,12 @@ pkgs.mkShell {
 
     glib
     libunwind
+    freetype
+    fontconfig
+    libGL
+    xorg.libX11
+    openal
+    ncurses
 
     # keep this line if you use bash
     bashInteractive
