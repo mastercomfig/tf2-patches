4a5850adda3ae156e2fc81b7c283e21d760ed0ee
Margen67 <Margen67@users.noreply.github.com>
2020-09-16 07:25:00 -0700

    Fix download_libs.bat
    Add @ to clean up output.
    The quotes were being included in the url, causing it to fail.
    Additionally, curl didn't create the needed directories, so it wouldn't have worked anyway.

--- download_libs.bat
+++ download_libs.bat
@@ -1,7 +1,7 @@
-set libs="lib/public/dme_controls.lib" "lib/common/win32/2015/debug/cryptlib.lib" "lib/common/win32/2015/release/cryptlib.lib" "lib/common/x64/2015/debug/cryptlib.lib" "lib/common/x64/2015/release/cryptlib.lib"
-set server="https://libs.mastercomfig.com/"
+@SET libs=lib/public/dme_controls.lib lib/common/win32/2015/debug/cryptlib.lib lib/common/win32/2015/release/cryptlib.lib lib/common/x64/2015/debug/cryptlib.lib lib/common/x64/2015/release/cryptlib.lib
+@SET server=https://libs.mastercomfig.com/
 
 (for %%l in (%libs%) do (
-   curl %server%%%l -o %%l
+   curl %server%%%l -Lo %%l --create-dirs
 ))
-PAUSE
+@PAUSE
