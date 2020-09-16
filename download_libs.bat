ad3567397fc072c0e7c6228b2d7b4435fb8f8f71
mastercoms <mastercoms@tuta.io>
2020-09-16 03:22:51 -0400

    literally add every lib

--- download_libs.bat
+++ download_libs.bat
@@ -1,4 +1,4 @@
-set libs="lib/public/dme_controls.lib"
+set libs="lib/public/dme_controls.lib" "lib/common/win32/2015/debug/cryptlib.lib" "lib/common/win32/2015/release/cryptlib.lib" "lib/common/x64/2015/debug/cryptlib.lib" "lib/common/x64/2015/release/cryptlib.lib"
 set server="https://libs.mastercomfig.com/"
 
 (for %%l in (%libs%) do (
