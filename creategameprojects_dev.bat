2dce98457f68e7c414a1e6b7d0a50d26e0229c3c
Nicolas Aragone <niquito.larigone@gmail.com>
2020-09-16 13:01:07 -0300

    Improved stealth, alternative to read output so CI doesn't hang.

--- creategameprojects_dev.bat
+++ creategameprojects_dev.bat
@@ -1,3 +1,3 @@
-set VALVE_NO_AUTO_P4=1
+@set VALVE_NO_AUTO_P4=1
 devtools\bin\vpc.exe /2015 /define:WORKSHOP_IMPORT_DISABLE /define:SIXENSE_DISABLE /define:NO_X360_XDK /define:RAD_TELEMETRY_DISABLED /define:DISABLE_ETW /define:LTCG /no_ceg /retail /define:CERT /nofpo /tf +game /nop4add /mksln games.sln
-PAUSE
+@timeout /t 30
