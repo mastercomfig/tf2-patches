882d22787a74d98b5fe2a792c3a59faace5d26d4
HurricanePootis <eatabakhsh04@protonmail.com>
2020-09-16 13:42:59 -0500

    fixed typo

--- creategameprojects.sh
+++ creategameprojects.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-chmod u+x ./devtools/bin/vpc_linux 
+chmod u+x ./devtools/bin/vpc2_linux
 devtools/bin/vpc2_linux /2015 /define:WORKSHOP_IMPORT_DISABLE /define:SIXENSE_DISABLE /define:NO_X360_XDK /define:RAD_TELEMETRY_DISABLED /define:DISABLE_ETW /define:LTCG /no_ceg /retail /define:CERT /tf +game /mksln games.mak
