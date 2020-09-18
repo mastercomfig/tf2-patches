4ac3cf9ca849c61f25a0b31da3c00401e9231d67
melvyn2 <melvyn.depeyrot@gmail.com>
2020-09-17 14:43:16 -1000

    Create debug vpc batch without retail, LTCG, or CERT

---
+++ creategameprojects_debug.bat
@@ -0,0 +1,2 @@
+devtools\bin\vpc.exe /2015 /define:WORKSHOP_IMPORT_DISABLE /define:SIXENSE_DISABLE /define:NO_X360_XDK /define:RAD_TELEMETRY_DISABLED /define:DISABLE_ETW /no_ceg /nofpo /tf +game /mksln games.sln
+@PAUSE
