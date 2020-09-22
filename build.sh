d4fbdfc53abf6f2e8dc675cc175192cf302a6035
melvyn2 <melvyn.depeyrot@gmail.com>
2020-09-22 07:22:43 -1000

    Clean up build.sh, authored by аааааааааааааааааааааааааааааааа#7660 (User ID: 490616977811963916)
    + Conditional error fix, by melvyn2

--- build.sh
+++ build.sh
@@ -9,9 +9,7 @@ if pwd | grep -q " "; then
 	exit 1
 fi
 
-if [ ! -v CORES ]; then
-	CORES=$(nproc)
-fi
+: "${CORES:=$(nproc)}"
 
 if [ ! -f ./thirdparty/gperftools-2.0/.libs/libtcmalloc_minimal.so ]; then
 	cd ./thirdparty/gperftools-2.0
@@ -52,36 +50,45 @@ if [ ! -f ./devtools/bin/vpc_linux ]; then
 	cd ../../../..
 fi
 
-if [ "$1" = '-dd' ]; then
-	echo -------------------------------------------------
-	echo                     WARNING
-	echo     You are probably about to waste your time
-	echo        Debug builds currently don\'t work
-	echo    You\'ll probably get an error about vphysics
-	echo -------------------------------------------------
-	sleep 2
+case "$1" in
+	-dd)
+		echo -------------------------------------------------
+		echo                     WARNING
+		echo     You are probably about to waste your time
+		echo        Debug builds currently don\'t work
+		echo    You\'ll probably get an error about vphysics
+		echo -------------------------------------------------
+		sleep 2
 
-	./creategameprojects_debug.sh
-elif [ "$1" = '-d' ]; then
-	./creategameprojects_dev.sh
-else
-	./creategameprojects.sh
-fi
+		./creategameprojects_debug.sh
+		;;
+	-d)
+		./creategameprojects_dev.sh
+		;;
+	*)
+		./creategameprojects.sh
+		;;
+esac
 
-if [ "$*" = '-c' ]; then
+if echo "$*" | grep -q -- "-c"; then
 	MAKEARGS="clean"
 else
 	MAKEARGS="all"
 fi
 
 export VALVE_NO_AUTO_P4=1
-if [ "$*" = '-vv' ]; then
-	make NO_CHROOT=1 STEAM_RUNTIME_PATH='' MAKE_JOBS=1 MAKE_VERBOSE=1 -f games.mak "$MAKEARGS"
-elif [ "$*" = '-v' ]; then
-	make NO_CHROOT=1 STEAM_RUNTIME_PATH='' MAKE_JOBS=1 -f games.mak "$MAKEARGS"
-else
-	CFLAGS="-w" CXXFLAGS="-w" make NO_CHROOT=1 STEAM_RUNTIME_PATH='' MAKE_JOBS="$CORES" -f games.mak "$MAKEARGS"
-fi
+
+case "$*" in
+	-vv)
+		make NO_CHROOT=1 STEAM_RUNTIME_PATH='' MAKE_JOBS=1 MAKE_VERBOSE=1 -f games.mak "$MAKEARGS"
+		;;
+	-v)
+		make NO_CHROOT=1 STEAM_RUNTIME_PATH='' MAKE_JOBS=1 -f games.mak "$MAKEARGS"
+		;;
+	*)
+		CFLAGS="-w" CXXFLAGS="-w" make NO_CHROOT=1 STEAM_RUNTIME_PATH='' MAKE_JOBS="$CORES" -f games.mak "$MAKEARGS"
+		;;
+esac
 
 cp game_clean/copy/bin/*.so ../game/bin/
 cp -r game_clean/copy/tf/custom/* ../game/tf/custom/
