d3b6580770e49a7d0b01ca8d1ee4561d318ffe02
melvyn2 <melvyn.depeyrot@gmail.com>
2020-09-25 05:53:10 -1000

    /bin/sh is not guaranteed to exist, use /usr/bin/env sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -e	# Stop on error
 cd "$(dirname "$0")"
 
--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 export LD_LIBRARY_PATH="./bin:./tf/bin:${LD_LIBRARY_PATH}"
 echo "LD_LIBRARY_PATH = ${LD_LIBRARY_PATH}"
-if [[ $1 == -d ]]; then
+
+if [ "$1" = '-d' ]; then
   cd ../game && lldb ./hl2_linux -- -steam -game tf -insecure -novid -nojoy -nosteamcontroller -nohltv -particles 1 -noborder -particle_fallback 2 -dev -allowdebug
 else
   cd ../game && ./hl2_linux -steam -game tf -insecure -novid -nojoy -nosteamcontroller -nohltv -particles 1 -noborder -particle_fallback 2 -dev -allowdebug
