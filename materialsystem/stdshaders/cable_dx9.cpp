36e4933e4adbf0b71b293bedf14b83d4665afbba
mastercoms <mastercoms@tuta.io>
2020-10-06 16:39:00 -0400

    fix missing shaders

--- materialsystem/stdshaders/cable_dx9.cpp
+++ materialsystem/stdshaders/cable_dx9.cpp
@@ -8,6 +8,7 @@
 
 #include "BaseVSShader.h"
 
+#include "Cable.inc"
 #include "cable_vs20.inc"
 #include "cable_ps20.inc"
 #include "cable_ps20b.inc"
@@ -30,8 +31,7 @@ BEGIN_VS_SHADER( Cable_DX9,
 
 	SHADER_FALLBACK
 	{
-		if ( !(g_pHardwareConfig->SupportsPixelShaders_2_0() && g_pHardwareConfig->SupportsVertexShaders_2_0()) ||
-				(g_pHardwareConfig->GetDXSupportLevel() < 90) )
+		if ( g_pHardwareConfig->GetDXSupportLevel() < 90 )
 		{
 			return "Cable_DX8";
 		}
@@ -73,18 +73,23 @@ BEGIN_VS_SHADER( Cable_DX9,
 				VERTEX_POSITION | VERTEX_COLOR | VERTEX_TANGENT_S | VERTEX_TANGENT_T, 
 				2, tCoordDimensions, 0 );
 
-			DECLARE_STATIC_VERTEX_SHADER( cable_vs20 );
-			SET_STATIC_VERTEX_SHADER( cable_vs20 );
+			DECLARE_STATIC_VERTEX_SHADER(cable);
+			SET_STATIC_VERTEX_SHADER(cable);
 
 			if( g_pHardwareConfig->SupportsPixelShaders_2_b() )
 			{
 				DECLARE_STATIC_PIXEL_SHADER( cable_ps20b );
 				SET_STATIC_PIXEL_SHADER( cable_ps20b );
 			}
-			else
+			else if (g_pHardwareConfig->SupportsPixelShaders_2_0())
 			{
 				DECLARE_STATIC_PIXEL_SHADER( cable_ps20 );
 				SET_STATIC_PIXEL_SHADER( cable_ps20 );
+			}
+			else if (g_pHardwareConfig->SupportsVertexShaders_2_0())
+			{
+				DECLARE_STATIC_VERTEX_SHADER(cable_vs20);
+				SET_STATIC_VERTEX_SHADER(cable_vs20);
 			}
 
 			// we are writing linear values from this shader.
@@ -118,9 +123,9 @@ BEGIN_VS_SHADER( Cable_DX9,
 			vEyePos_SpecExponent[3] = 0.0f;
 			pShaderAPI->SetPixelShaderConstant( PSREG_EYEPOS_SPEC_EXPONENT, vEyePos_SpecExponent, 1 );
 
-			DECLARE_DYNAMIC_VERTEX_SHADER( cable_vs20 );
-			SET_DYNAMIC_VERTEX_SHADER_COMBO( DOWATERFOG, pShaderAPI->GetSceneFogMode() == MATERIAL_FOG_LINEAR_BELOW_FOG_Z );
-			SET_DYNAMIC_VERTEX_SHADER( cable_vs20 );
+			DECLARE_DYNAMIC_VERTEX_SHADER(cable);
+			SET_DYNAMIC_VERTEX_SHADER_COMBO(DOWATERFOG, pShaderAPI->GetSceneFogMode() == MATERIAL_FOG_LINEAR_BELOW_FOG_Z);
+			SET_DYNAMIC_VERTEX_SHADER(cable);
 
 			if( g_pHardwareConfig->SupportsPixelShaders_2_b() )
 			{
@@ -129,11 +134,17 @@ BEGIN_VS_SHADER( Cable_DX9,
 				SET_DYNAMIC_PIXEL_SHADER_COMBO( WRITE_DEPTH_TO_DESTALPHA, bFullyOpaque && pShaderAPI->ShouldWriteDepthToDestAlpha() );
 				SET_DYNAMIC_PIXEL_SHADER( cable_ps20b );
 			}
-			else
+			else if (g_pHardwareConfig->SupportsPixelShaders_2_0())
 			{
 				DECLARE_DYNAMIC_PIXEL_SHADER( cable_ps20 );
 				SET_DYNAMIC_PIXEL_SHADER_COMBO( PIXELFOGTYPE, pShaderAPI->GetPixelFogCombo() );
 				SET_DYNAMIC_PIXEL_SHADER( cable_ps20 );
+			}
+			else if (g_pHardwareConfig->SupportsVertexShaders_2_0())
+			{
+				DECLARE_DYNAMIC_VERTEX_SHADER(cable_vs20);
+				SET_DYNAMIC_VERTEX_SHADER_COMBO(DOWATERFOG, pShaderAPI->GetSceneFogMode() == MATERIAL_FOG_LINEAR_BELOW_FOG_Z);
+				SET_DYNAMIC_VERTEX_SHADER(cable_vs20);
 			}
 		}
 		Draw();
