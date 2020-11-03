50ab14e9062284329651a05ea63a027a83f6d7d8
melvyn2 <melvyn.depeyrot@gmail.com>
2020-11-03 12:51:40 -0800

    Remove unneeded library path, add benchmark mode

--- run.sh
+++ run.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -e
 cd "$(dirname "$0")"/../game
-pwd
-# shellcheck disable=SC2155  # it would be a very strange day indeed if pwd fails
-export LD_LIBRARY_PATH="/usr/lib32:$(pwd)/bin:$(pwd)/tf/bin:${LD_LIBRARY_PATH}"
-echo "LD_LIBRARY_PATH = ${LD_LIBRARY_PATH}"
 
 MANGO="$(command -v mangohud || true)"
 ARGS="-steam -game tf -insecure -novid -nojoy -nosteamcontroller -nohltv -particles 1 -noborder -particle_fallback 2 -dev"
@@ -12,6 +8,12 @@ ARGS="-steam -game tf -insecure -novid -nojoy -nosteamcontroller -nohltv -partic
 if [[ "$1" == "-d" ]]; then
 	# shellcheck disable=SC2086
 	lldb ./hl2_linux -- -allowdebug ${ARGS}
+elif [[ "$1" == "-b" ]]; then
+	rm tf/cfg/config.cfg
+	mkdir -p ../benchlogs
+	# shellcheck disable=SC2086
+	MANGOHUD_DLSYM=1 MANGOHUD_CONFIG="output_folder=$(pwd)/../benchlogs,toggle_logging=F12" mangohud \
+		"$(pwd)"/hl2_linux ${ARGS} "+bind f12 demo_resume" "+fps_max 0" "+playdemo demos/tc2suijin" "+demo_pause"
 else
 	# shellcheck disable=SC2086
 	MANGOHUD_DLSYM=1 ${MANGO} "$(pwd)"/hl2_linux ${ARGS}
