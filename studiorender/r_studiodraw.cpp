cdf4c335622a64f266776b869c242aebb2c84cdc
Tsarevich Dmitry <dimhotepus@gmail.com>
2020-12-14 00:20:42 +0300

    x86-64/studiorender: Port to x86-64
    * Use C++ implementation of ComputeSkinMatrixSSE,
      as any 64 bit compiler involved (gcc, clang, msvc) already
      emits SSE2 instructions.  And MSVC doesn't support inline
      asm in x86-64 mode.

--- studiorender/r_studiodraw.cpp
+++ studiorender/r_studiodraw.cpp
@@ -658,7 +658,7 @@ static matrix3x4_t *ComputeSkinMatrix( mstudioboneweight_t &boneweights, matrix3
 static matrix3x4_t *ComputeSkinMatrixSSE( mstudioboneweight_t &boneweights, matrix3x4_t *pPoseToWorld, matrix3x4_t &result )
 {
 	// NOTE: pPoseToWorld, being cache aligned, doesn't need explicit initialization
-#if defined( _WIN32 ) && !defined( _X360 )
+#if defined( _WIN32 ) && !defined (_WIN64) && !defined( _X360 )
 	switch( boneweights.numbones )
 	{
 	default:
@@ -867,8 +867,8 @@ static matrix3x4_t *ComputeSkinMatrixSSE( mstudioboneweight_t &boneweights, matr
 		return &result;
 #endif
 	}
-#elif POSIX
-#warning "ComputeSkinMatrixSSE C implementation only"
+#elif POSIX || defined(_WIN64)
+#pragma message ("WARN: ComputeSkinMatrixSSE C++ implementation only")
 	return ComputeSkinMatrix( boneweights, pPoseToWorld, result );
 #elif defined( _X360 )
 	return ComputeSkinMatrix( boneweights, pPoseToWorld, result );
