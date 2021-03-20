385ff850614a53cd73ad3de19eae4dc087586f89
Tsarevich Dmitry <dimhotepus@gmail.com>
2021-03-20 19:16:28 +0300

    filesystem_init: Init member by default to prevent UB

--- public/filesystem_init.cpp
+++ public/filesystem_init.cpp
@@ -223,6 +223,7 @@ CFSSearchPathsInit::CFSSearchPathsInit()
 {
 	m_pDirectoryName = NULL;
 	m_pLanguage = NULL;
+	m_pFileSystem = NULL;
 	m_ModPath[0] = 0;
 	m_bMountHDContent = m_bLowViolence = false;
 }
@@ -234,13 +235,16 @@ CFSSteamSetupInfo::CFSSteamSetupInfo()
 	m_bOnlyUseDirectoryName = false;
 	m_bSteam = false;
 	m_bToolsMode = true;
+	m_bSetSteamDLLPath = false;
 	m_bNoGameInfo = false;
+	m_GameInfoPath[0] = 0;
 }
 
 
 CFSLoadModuleInfo::CFSLoadModuleInfo()
 {
 	m_pFileSystemDLLName = NULL;
+	m_ConnectFactory = NULL;
 	m_pFileSystem = NULL;
 	m_pModule = NULL;
 }
