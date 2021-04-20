3599c1321b35316b85d3a66d10f14ece5c277738
Tsarevich Dmitry <dimhotepus@gmail.com>
2021-04-21 00:25:28 +0300

    [vpc_scripts] Use C++17 on Windows by default
    * Use C++17 as vs project language.  Required as sources
      already in C++17.
    * Cleanup generated by VPC project files - use already
      set by MSVC defaults instead of setting them manually to
      shrink generated project files.

--- creategameprojects.bat
+++ creategameprojects.bat
@@ -1,2 +1,3 @@
 @echo OFF
+msbuild /m /p:Platform="x86";Configuration=Release external/vpc/vpc.sln
 devtools\bin\vpc.exe /2015 /define:WORKSHOP_IMPORT_DISABLE /define:SIXENSE_DISABLE /define:NO_X360_XDK /define:RAD_TELEMETRY_DISABLED /define:DISABLE_ETW /define:LTCG /no_ceg /retail /define:CERT /tf +game /mksln games.sln
