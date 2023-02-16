e5c9254ce003e6472f4681185a191e538d392169
mastercoms <mastercoms@tuta.io>
2023-02-16 00:00:29 -0500

    revert back to GC item schema

--- game/shared/econ/econ_item_system.cpp
+++ game/shared/econ/econ_item_system.cpp
@@ -492,18 +492,9 @@ public:
 #if ( defined( GAME_DLL ) || defined( CLIENT_DLL ) ) && ( defined( _DEBUG ) || defined( STAGING_ONLY ) )
 		const bool bUseGCCopy = items_game_use_gc_copy.GetBool();
 #else
-#ifdef VALVE_PURE
 		const bool bUseGCCopy = true;
-#else
-		const bool bUseGCCopy = false;
 #endif
-#endif
-
-#ifdef VALVE_PURE
 		if ( bUseGCCopy == false && k_EUniversePublic != GetUniverse() )
-#else
-		if ( bUseGCCopy == false )
-#endif
 		{
 			Msg( "Loading item schema from local file.\n" );
 			KeyValuesAD pItemsGameKV( "ItemsGameFile" );
