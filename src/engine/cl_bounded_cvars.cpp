3324f6e210558878d1b9125f450095dd7c52843b
mastercoms <mastercoms@tuta.io>
2023-03-10 22:09:04 -0500

    fix: update networking setting defaults
    * bump up updaterate and cmdrate to be equal and more closely
      aligned to the tick interval. this is a slight increase so it
      should not adversely affect players in terms of bandwidth and processing
      time. the fact that the rates are equal means that we aren't giving
      an imbalance of data for the player to process and repredict on.
    * fix an issue with updaterate being limited to 100, when some servers
      use a 133 tickrate value. this is similar to how CSGO increased their
      internal updaterate limit to 128. I also took the opportunity to
      limit the snapshot interval according to the tickrate, which prevents
      networking errors caused by bad networking settings on both the client
      and server.
    * limit the value of cl_interp to 0.2 instead of 0.5
      since players were exploiting this to desync/backtrack.
    * set sv_maxunlag to 0.5. since cl_interp's max value was reduced,
      we can set the unlag window to be shorter. CSGO sets this to 0.2
      but that is much too aggressive, considering interp. 0.7 would be
      a value to decrease by for just the interp limit, but I have decreased
      by a further 0.2s ping to reduce the unlag window for laggy players
      and cheaters who are using this mechanic to backtrack.
    * raise bandwidth rate from 80000 to 131072. this is the maximum value
      that seems to work well for most users across a variety of connections
      and routers, possibly due to some buffers or misconfigurations.
      196608 was tested over from CSGO but this had issues for some connections.
      this increase of rate ensures there are no delays with larger net transfers
      that usually occur in certain situations, such as when a player
      is teleported, spawned or switches spectator targets.
    * enforce a minrate of the old default rate, to prevent choking.
      an alternative value would be 16000 which is the lowest value selectable
      in CSGO, but commonly configs set this to 42,000 or 60,000 for some
      reason, so I wanted to take the opportunity to raise the standard here.
    * enforce a max rate of the highest rate selectable in CSGO
      anything higher seems to cause problems with networking
    * net_maxpacketdrop is set to 0. I'm not clued in why this was added
      in the first place, but effectively what this does is force you to
      drop even more packets whenever you drop packets from lossy internet.
    * also adjust sv_client_max_interp_ratio so that server owners cannot
      force clients to use a low interp value (ratio = 1). with this adjustment,
      sv_client_max_interp_ratio can now be safely enforced for cl_interp as well.
    * update max routable to be inline with steam sockets max recommended size
    * finally, adjust a few server networking cvars to be tighter and closer
      with CSGO. this is to prevent players from exploiting these values
      to desync/backtrack. it isn't too tight to punish worse connections too much,
      and the tightness can improve hitreg.

--- src/engine/cl_bounded_cvars.cpp
+++ src/engine/cl_bounded_cvars.cpp
@@ -72,7 +72,7 @@ public:
 	CBoundedCvar_CmdRate() :
 	  ConVar_ServerBounded( 
 		  "cl_cmdrate", 
-		  "30", 
+		  "33", 
 		  FCVAR_ARCHIVE | FCVAR_USERINFO, 
 		  "Max number of command packets sent to server per second", true, MIN_CMD_RATE, true, MAX_CMD_RATE )
 	{
@@ -119,7 +119,7 @@ public:
 	CBoundedCvar_UpdateRate() :
 	  ConVar_ServerBounded( 
 		  "cl_updaterate",
-		  "20", 
+		  "33", 
 		  FCVAR_ARCHIVE | FCVAR_USERINFO | FCVAR_NOT_CONNECTED, 
 		  "Number of packets per second of updates you are requesting from the server" )
 	{
