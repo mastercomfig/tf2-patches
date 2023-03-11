002e01f721bf06b4dd5333e2224dbaca5102cd72
mastercoms <mastercoms@tuta.io>
2023-03-10 22:09:04 -0500

    fix: update networking setting defaults
    pending explanation

--- src/engine/net.h
+++ src/engine/net.h
@@ -21,7 +21,7 @@
 // Flow control bytes per second limits
 #define MAX_RATE		(1024*1024)				
 #define MIN_RATE		1000
-#define DEFAULT_RATE	80000
+#define DEFAULT_RATE	131072
 
 #define SIGNON_TIME_OUT				300.0f  // signon disconnect timeout
 
