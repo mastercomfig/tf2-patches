23e358294cfc5e0dae6491ebfd8daa2382b824cd
mastercoms <mastercoms@tuta.io>
2023-03-13 12:25:41 -0400

    perf: disable COM_TimestampedLog
    within threaded particles, there was a lot of traffic on a log
    mutex, and that was slowing things down significantly
    
    I'm not exactly sure why this was triggering, as the code
    seems to prevent this from going off unless
    there is a -profile or -etwprofile argument, will investigate
    later.
    
    another option is also removing the COM_TimestampedLog
    from just the particles function, but who knows what else
    this could be slowing down

--- src/tier0/dbg.cpp
+++ src/tier0/dbg.cpp
@@ -870,6 +870,7 @@ void ValidateSpew( CValidator &validator )
 //-----------------------------------------------------------------------------
 void COM_TimestampedLog( char const *fmt, ... )
 {
+#ifdef _DEBUG
 	static float s_LastStamp = 0.0;
 	static bool s_bShouldLog = false;
 	static bool s_bShouldLogToETW = false;
@@ -925,6 +926,7 @@ void COM_TimestampedLog( char const *fmt, ... )
 	}
 
 	s_LastStamp = curStamp;
+#endif
 }
 
 //-----------------------------------------------------------------------------
