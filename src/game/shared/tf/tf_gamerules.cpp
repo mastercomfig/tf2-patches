d30c75b0e445b0c162ef5748059efa73a0e1e0a3
mastercoms <mastercoms@tuta.io>
2023-04-09 14:49:04 -0400

    gameplay: fix enforcer pierce bugs
    fix a seeming typo on resistance piercing for the battallion's backup.
    there is no evidence I could find that mod_ignore_resists_absorbs ever existed
    as a valid attribute, so change it to the correct mod_pierce_resists_absorbs
    that is used by the enforcer. this was already done after Jungle Inferno,
    but the part which skipped over crits was missed (maybe intentionally)
    but I've included both parts here.
    
    also add an exclusion for the mult_dmgtaken_from_bullets. the Pain Train
    uses this to add bullet vulnerability, so the enforcer actually does less damage
    in this case since it "pierces" the vulnerability. technically, the pierce resistance
    attribute should also do this check for all other damage types for compatibility
    with attribute mods, but that's a more extensive change than just addressing it
    for the vanilla case.

--- src/game/shared/tf/tf_gamerules.cpp
+++ src/game/shared/tf/tf_gamerules.cpp
@@ -6186,7 +6186,7 @@ bool CTFGameRules::ApplyOnDamageModifyRules( CTakeDamageInfo &info, CBaseEntity 
 	}
 
 	int iPierceResists = 0;
-	CALL_ATTRIB_HOOK_INT_ON_OTHER( pWeapon, iPierceResists, mod_ignore_resists_absorbs );
+	CALL_ATTRIB_HOOK_INT_ON_OTHER( pWeapon, iPierceResists, mod_pierce_resists_absorbs );
 
 	// Use defense buffs if it's not a backstab or direct crush damage (telefrage, etc.)
 	if ( pVictim && info.GetDamageCustom() != TF_DMG_CUSTOM_BACKSTAB && ( info.GetDamageType() & DMG_CRUSH ) == 0 )
@@ -6836,10 +6836,11 @@ float CTFGameRules::ApplyOnDamageAliveModifyRules( const CTakeDamageInfo &info, 
 		// Check if we're immune
 		outParams.bPlayDamageReductionSound = CheckForDamageTypeImmunity( info.GetDamageType(), pVictim, flDamageBase, flDamageBonus );
 
+		// Reduce only the crit portion of the damage with crit resist
+			bool bCrit = ( info.GetDamageType() & DMG_CRITICAL ) > 0;
+
 		if ( !iPierceResists )
 		{
-			// Reduce only the crit portion of the damage with crit resist
-			bool bCrit = ( info.GetDamageType() & DMG_CRITICAL ) > 0;
 			if ( bCrit )
 			{
 				// Break the damage down and reassemble
@@ -6879,15 +6880,23 @@ float CTFGameRules::ApplyOnDamageAliveModifyRules( const CTakeDamageInfo &info, 
 					outParams.bPlayDamageReductionSound = CheckMedicResist( TF_COND_MEDIGUN_SMALL_BLAST_RESIST, TF_COND_MEDIGUN_UBER_BLAST_RESIST, pVictim, flRawDamage, flDamageBase, bCrit, flDamageBonus );
 				}
 			}
+		}
 
-			if ( info.GetDamageType() & (DMG_BULLET|DMG_BUCKSHOT) )
+		if ( info.GetDamageType() & (DMG_BULLET|DMG_BUCKSHOT) )
+		{
+			float flResist = 1.0f;
+			CALL_ATTRIB_HOOK_FLOAT_ON_OTHER( pVictim, flResist, mult_dmgtaken_from_bullets );
+			// If the resist is actually a vulnerability, apply it even if we're piercing resists
+			if ( flResist > 1.0f || !iPierceResists )
 			{
-				CALL_ATTRIB_HOOK_FLOAT_ON_OTHER( pVictim, flDamageBase, mult_dmgtaken_from_bullets );
-
+				flDamageBase *= flResist;
 				// Check for medic resist
 				outParams.bPlayDamageReductionSound = CheckMedicResist( TF_COND_MEDIGUN_SMALL_BULLET_RESIST, TF_COND_MEDIGUN_UBER_BULLET_RESIST, pVictim, flRawDamage, flDamageBase, bCrit, flDamageBonus );
 			}
+		}
 
+		if ( !iPierceResists )
+		{
 			if ( info.GetDamageType() & DMG_MELEE )
 			{
 				CALL_ATTRIB_HOOK_FLOAT_ON_OTHER( pVictim, flDamageBase, mult_dmgtaken_from_melee );
