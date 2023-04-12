52be410e10e0ea9efbb0dd74867b4831d6abc1b5
mastercoms <mastercoms@tuta.io>
2023-04-12 13:58:39 -0400

    gameplay: explosive headshot does bullet damage instance
    Historically, explosive headshot was a damage over time applied
    as bleed damage, but was then changed to deal a single damage
    instance. Bleed ignores resistances, including not counting as ranged
    damage for Fists of Steel heavies, which some people say makes
    this mechanic problematic. However, this does change damage values
    and effectiveness of Sniper in MvM so this may be ignored if not desired.
    
    ref: https://github.com/ValveSoftware/Source-1-Games/issues/4619

--- src/game/shared/tf/tf_weapon_sniperrifle.cpp
+++ src/game/shared/tf/tf_weapon_sniperrifle.cpp
@@ -1161,8 +1161,10 @@ void CTFSniperRifle::ExplosiveHeadShot( CTFPlayer *pAttacker, CTFPlayer *pVictim
 		flStunAmt = pTFPlayer->IsMiniBoss() ? 0.5f : RemapValClamped( iExplosiveShot, 1, 3, 0.5f, 0.8f );
 		pTFPlayer->m_Shared.StunPlayer( flStunDuration, flStunAmt, TF_STUN_MOVEMENT, pAttacker );
 
-		// DoT
-		pTFPlayer->m_Shared.MakeBleed( pAttacker, this, 0.1f, flDmg );
+		// Radial damage
+		CTakeDamageInfo info( this, pAttacker, NULL, flDmg, DMG_BULLET );
+		info.SetDamageCustom( TF_DMG_CUSTOM_NONE );
+		pTFPlayer->TakeDamage( info );
 
 		// Shoot a beam at them
 		CPVSFilter filter( pTFPlayer->WorldSpaceCenter() );
