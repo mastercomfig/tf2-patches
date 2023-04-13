f69f798e35d3892e542925f45dd0ddcfbb99c645
mastercoms <mastercoms@tuta.io>
2023-04-13 18:47:40 -0400

    gameplay: fix reflect bugs and related projectile bugs
    This was a doozy. I tried scanning through the codebase for
    all usages and interactions to make sure everything was working
    properly, and also tested the interactions.
    
    * Fix reflected sandman balls not dealing damage
    * Fix reflected ornament not dealing damage/bleed properly
    * Fix Scout playing hit/miss voicelines on reflected balls
    * Fix some projectile's lifetime not being tied to the reflector
    * Fix reflected rescue ranger not healing buildings
    * Fix reflected flares not exhibiting their correct properties
    * Fix reflected flares and sticky bombs lifetimes not being tracked properly
    * Fix projectiles not using the correct team color when reflected
    * Fix reflected air strike not showing the correct particles
    * Fix reflected sentry rockets causing flamethrower sound
    * Fix reflected projectiles not dealing correct damage
    * Fix reflected projectiles not having correct radius
    * Fix reflected projectiles not having correct speed in some cases
    * Fix reflected projectiles not contributing to buildings destroyed strange part
    * Fix reflected cannonballs not double donking
    * Fix sentry rockets/bullets not contributing to buildings destroyed strange part
    * Fix reflected bow having incorrect push force and other effects
    * Fix reflected projectiles not using the correct spells
    * Fix enemy Snipers receiving extinguish bonus from reflected Jarate
    * Fix kill icons for reflected ball, cleaver, flares, arrow headshot
      burning arrow, burning arrow headshot
    
    ref: https://www.youtube.com/watch?v=EDsDnR2dzlw
    ref: https://www.youtube.com/watch?v=NxeljS6GE9g
    ref: https://github.com/ValveSoftware/Source-1-Games/issues/4298
    ref: https://github.com/ValveSoftware/Source-1-Games/issues/4171
    ref: https://github.com/ValveSoftware/Source-1-Games/issues/3368
    ref: https://github.com/ValveSoftware/Source-1-Games/issues/3292

--- src/game/shared/tf/tf_gamerules.cpp
+++ src/game/shared/tf/tf_gamerules.cpp
@@ -5898,15 +5898,18 @@ bool CTFGameRules::ApplyOnDamageModifyRules( CTakeDamageInfo &info, CBaseEntity 
 					info.SetDamage( 400.0f );
 				}
 			}
-			else if( pTFAttacker && pWeapon && pWeapon->GetWeaponID() == TF_WEAPON_CANNON && ( info.GetDamageType() & DMG_BLAST ) )
+			else if( pTFAttacker && pBaseGrenade && pBaseGrenade->GetType() == TF_GL_MODE_CANNONBALL && ( info.GetDamageType() & DMG_BLAST ) )
 			{
-				CTFGrenadeLauncher* pGrenadeLauncher = static_cast<CTFGrenadeLauncher*>( pWeapon );
+				CTFGrenadeLauncher* pGrenadeLauncher = static_cast<CTFGrenadeLauncher*>( pBaseGrenade->GetOriginalLauncher() );
 				if( pGrenadeLauncher->IsDoubleDonk( pVictim ) )
 				{
 					info.SetCritType( CTakeDamageInfo::CRIT_MINI );
 					eBonusEffect = kBonusEffect_DoubleDonk;
 					info.SetDamage( info.GetMaxDamage() ); // Double donk victims score max damage
-					EconEntity_OnOwnerKillEaterEvent( pGrenadeLauncher, pTFAttacker, pVictim, kKillEaterEvent_DoubleDonks );
+					if ( pWeapon && pWeapon->GetWeaponID() == TF_WEAPON_CANNON )
+					{
+						EconEntity_OnOwnerKillEaterEvent( pWeapon, pTFAttacker, pVictim, kKillEaterEvent_DoubleDonks );
+					}
 				}
 			}
 			else
@@ -10154,7 +10157,12 @@ CTFWeaponBase *GetKilleaterWeaponFromDamageInfo( const CTakeDamageInfo *pInfo )
 				CTFLaserPointer* pLaserPointer = dynamic_cast< CTFLaserPointer * >( pBuilder->GetEntityForLoadoutSlot( LOADOUT_POSITION_SECONDARY ) );
 				if ( pLaserPointer && pLaserPointer->HasLaserDot() )
 				{
-					pTFWeapon =  pLaserPointer;
+					pTFWeapon = pLaserPointer;
+				}
+				else
+				{
+					// Not a wrangler, credit the wrench. Technically we have to also credit the PDA, but the melee weapon is more relevant
+					pTFWeapon = dynamic_cast< CTFWeaponBase * >( pBuilder->GetEntityForLoadoutSlot( LOADOUT_POSITION_MELEE ) );
 				}
 			}
 		}
@@ -10164,24 +10172,11 @@ CTFWeaponBase *GetKilleaterWeaponFromDamageInfo( const CTakeDamageInfo *pInfo )
 	// deflection will get the killeater credit instead of the original launcher
 	if ( pInflictor )
 	{
-		CTFWeaponBaseGrenadeProj *pBaseGrenade = dynamic_cast< CTFWeaponBaseGrenadeProj* >( pInflictor );
-		if ( pBaseGrenade )
+		// GetLauncher always points to the weapon that launched, deflect or not. So this is safe.
+		CBaseProjectile *pProjectile = dynamic_cast< CBaseProjectile* >( pInflictor );
+		if ( pProjectile )
 		{
-			if ( pBaseGrenade->GetDeflected() && pBaseGrenade->GetLauncher() )
-			{
-				pTFWeapon = dynamic_cast< CTFWeaponBase* >( pBaseGrenade->GetLauncher() );
-			}
-		}
-		else
-		{
-			CTFBaseRocket *pRocket = dynamic_cast< CTFBaseRocket* >( pInflictor );
-			if ( pRocket )
-			{
-				if ( pRocket->GetDeflected() && pRocket->GetLauncher() )
-				{
-					pTFWeapon = dynamic_cast< CTFWeaponBase* >( pRocket->GetLauncher() );
-				}
-			}
+			pTFWeapon = dynamic_cast< CTFWeaponBase* >( pProjectile->GetLauncher() );
 		}
 	}
 
@@ -11166,11 +11161,18 @@ void CTFGameRules::PlayerKilled( CBasePlayer *pVictim, const CTakeDamageInfo &in
 	CTFPlayer *pTFPlayerVictim = ToTFPlayer( pVictim );
 	CTFPlayer *pTFPlayerScorer = ToTFPlayer( pScorer );
 	if ( pScorer )
-	{	
-		CalcDominationAndRevenge( pTFPlayerScorer, info.GetWeapon(), pTFPlayerVictim, false, &iDeathFlags );
+	{
+		// Make sure we're getting the right weapon (for reflects, sentries, etc)...
+		CBaseEntity *pAttackerEconWeapon = GetKilleaterWeaponFromDamageInfo( &info );
+		if ( !pAttackerEconWeapon )
+		{
+			// It might not be a TF weapon, but a wearable, so fallback to the raw info in that case
+			pAttackerEconWeapon = info.GetWeapon();
+		}
+		CalcDominationAndRevenge( pTFPlayerScorer, pAttackerEconWeapon, pTFPlayerVictim, false, &iDeathFlags );
 		if ( pAssister )
 		{
-			CalcDominationAndRevenge( pAssister, info.GetWeapon(), pTFPlayerVictim, true, &iDeathFlags );
+			CalcDominationAndRevenge( pAssister, pAttackerEconWeapon, pTFPlayerVictim, true, &iDeathFlags );
 		}
 	}
 	pTFPlayerVictim->SetDeathFlags( iDeathFlags );	
@@ -11769,6 +11771,8 @@ const char *CTFGameRules::GetKillingWeaponName( const CTakeDamageInfo &info, CTF
 		killer_weapon_name = "tf_weapon_flaregun";
 		*iWeaponID = TF_WEAPON_FLAREGUN;
 
+		bool bIsDeflected = false;
+
 		if ( pInflictor && pInflictor->IsPlayer() == false )
 		{
 			CTFBaseRocket *pBaseRocket = dynamic_cast<CTFBaseRocket*>( pInflictor );
@@ -11778,18 +11782,22 @@ const char *CTFGameRules::GetKillingWeaponName( const CTakeDamageInfo &info, CTF
 				if ( pBaseRocket->GetDeflected() )
 				{
 					killer_weapon_name = "deflect_flare";
+					bIsDeflected = true;
 				}
 			}
 		}
 
-		CTFWeaponBase *pWeapon = dynamic_cast< CTFWeaponBase * >( info.GetWeapon() );
-		if ( pWeapon && pWeapon->GetWeaponID() == TF_WEAPON_FLAREGUN_REVENGE )
+		if (!bIsDeflected)
 		{
-			CEconItemView *pItem = pWeapon->GetAttributeContainer()->GetItem();
-			if ( pItem && pItem->GetStaticData() && pItem->GetStaticData()->GetIconClassname() )
+			CTFWeaponBase *pWeapon = dynamic_cast< CTFWeaponBase * >( info.GetWeapon() );
+			if ( pWeapon && pWeapon->GetWeaponID() == TF_WEAPON_FLAREGUN_REVENGE )
 			{
-				killer_weapon_name = pItem->GetStaticData()->GetIconClassname();
-				*iWeaponID = pWeapon->GetWeaponID();
+				CEconItemView *pItem = pWeapon->GetAttributeContainer()->GetItem();
+				if ( pItem && pItem->GetStaticData() && pItem->GetStaticData()->GetIconClassname() )
+				{
+					killer_weapon_name = pItem->GetStaticData()->GetIconClassname();
+					*iWeaponID = pWeapon->GetWeaponID();
+				}
 			}
 		}
 	}
@@ -11805,7 +11813,7 @@ const char *CTFGameRules::GetKillingWeaponName( const CTakeDamageInfo &info, CTF
 			{
 				if ( pBaseRocket->GetDeflected() )
 				{
-					killer_weapon_name = "deflect_flare_detonator";
+					killer_weapon_name = "deflect_flare";
 				}
 			}
 		}
@@ -11900,6 +11908,20 @@ const char *CTFGameRules::GetKillingWeaponName( const CTakeDamageInfo &info, CTF
 	else if ( info.GetDamageCustom() == TF_DMG_CUSTOM_BASEBALL )
 	{
 		killer_weapon_name = "ball";
+
+		if ( pInflictor && pInflictor->IsPlayer() == false )
+		{
+			CTFWeaponBaseGrenadeProj *pBaseGrenade = dynamic_cast<CTFWeaponBaseGrenadeProj*>( pInflictor );
+			if ( pBaseGrenade && pBaseGrenade->GetDeflected() )
+			{
+				killer_weapon_name = "deflect_ball";
+			}
+		}
+	}
+	else if ( info.GetDamageCustom() == TF_DMG_CUSTOM_CLEAVER ||
+			  info.GetDamageCustom() == TF_DMG_CUSTOM_CLEAVER_CRIT )
+	{
+		killer_weapon_name = "guillotine";
 	}
 	else if ( info.GetDamageCustom() == TF_DMG_CUSTOM_COMBO_PUNCH )
 	{
@@ -12077,6 +12099,18 @@ const char *CTFGameRules::GetKillingWeaponName( const CTakeDamageInfo &info, CTF
 				else if ( *iWeaponID == TF_WEAPON_ROCKETLAUNCHER_DIRECTHIT )
 				{
 					killer_weapon_name = "rocketlauncher_directhit";
+				}
+				else if ( *iWeaponID == TF_WEAPON_COMPOUND_BOW )
+				{
+					CTFProjectile_Arrow* pArrow = dynamic_cast<CTFProjectile_Arrow*>( pBaseRocket );
+					if ( pArrow && pArrow->IsAlight() )
+					{
+						killer_weapon_name = "huntsman_flyingburn";
+
+						// force death notice to use burning arrow headshot kill icon
+						if ( info.GetDamageCustom() == TF_DMG_CUSTOM_HEADSHOT )
+							*iWeaponID = TF_WEAPON_NONE;
+					}
 				}
 			}
 			else
@@ -12713,7 +12747,16 @@ void CTFGameRules::DeathNotice( CBasePlayer *pVictim, const CTakeDamageInfo &inf
 				}
 				else
 				{
-					pKillStreakTarget = info.GetWeapon();
+					// If we are a projectile, we need to get the launcher, because the damage info might have been initialized with the original launcher.
+					CBaseProjectile *pProjectile = dynamic_cast<CBaseProjectile*>( pInflictor );
+					if ( pProjectile )
+					{
+						pKillStreakTarget = pProjectile->GetLauncher();
+					}
+					else
+					{
+						pKillStreakTarget = info.GetWeapon();
+					}
 				}
 
 				if ( pKillStreakTarget )
