71e668d99b1b0282f74c5bb88b410d0e70464712
mastercoms <mastercoms@tuta.io>
2024-01-14 21:10:52 -0500

    add start_tf2.sh

---
+++ game_clean/copy/start_tf2.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")"
+
+ARGS="-steam -game tf -insecure -novid -nojoy -nosteamcontroller -nobreakpad"
+
+export LAUNCH_PREFIX="$HOME/.steam/bin32/steam-runtime/run.sh"
+
+${LAUNCH_PREFIX} "$(pwd)"/hl2.sh ${ARGS} $@
